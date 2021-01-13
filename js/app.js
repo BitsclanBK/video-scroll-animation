f4a172490fd18ce6d8d6a375448e7f4802d42779
Sohaib Salman <sohaibsalman10@gmail.com>
2021-01-13 12:16:26 +0500

    remove second video

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
-// Get DOM elements
+// Get Section elements
 const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
-const text = intro.querySelector('h1');
 
-const section = document.querySelector('section');
-const end = section.querySelector('h1');
+// Get video elements
+const videoIntro = intro.querySelector('video');
 
 // Init ScrollMagic Code
 const controller = new ScrollMagic.Controller();
 
-const scene = new ScrollMagic.Scene({
-  duration: 7000,
+const sceneIntro = new ScrollMagic.Scene({
+  duration: 9000,
   triggerElement: intro,
   triggerHook: 0,
 })
   .setPin(intro)
+  .addIndicators()
   .addTo(controller);
 
 // Video animation
-let accelerationAmount = 0.1;
+let accelerationAmount = 0.5;
 let scrollPosition = 0;
 let delay = 0;
 
-scene.on('update', (e) => {
-  scrollPosition = e.scrollPos / 500;
+sceneIntro.on('update', (e) => {
+  scrollPosition = e.scrollPos / 1000;
 });
 
 setInterval(() => {
+  console.log('interval');
   delay += (scrollPosition - delay) * accelerationAmount;
-
-  video.currentTime = delay;
-}, 40);
+  videoIntro.currentTime = delay;
+}, 33.3);
