d753bcbd223dac6653c6e0bc026c839f955f3116
Sohaib Salman <sohaibsalman10@gmail.com>
2021-01-13 18:45:36 +0500

    Add support for second video

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ const sceneIntro = new ScrollMagic.Scene({
   triggerHook: 0,
 })
   .setPin(intro)
-  .addIndicators()
   .addTo(controller);
 
 // Video animation
@@ -23,10 +22,40 @@ let delay = 0;
 
 sceneIntro.on('update', (e) => {
   scrollPosition = e.scrollPos / 1000;
-});
-
-setInterval(() => {
-  console.log('interval');
   delay += (scrollPosition - delay) * accelerationAmount;
   videoIntro.currentTime = delay;
-}, 33.3);
+});
+
+// INTRO 2
+
+// Get Section elements
+const intro2 = document.querySelector('.intro2');
+
+// Get video elements
+const videoIntro2 = intro2.querySelector('video');
+
+// Init ScrollMagic Code
+const controller2 = new ScrollMagic.Controller();
+
+const sceneIntro2 = new ScrollMagic.Scene({
+  duration: 7000,
+  triggerElement: intro2,
+  triggerHook: 0,
+})
+  .setPin(intro2)
+  .addTo(controller2);
+
+// Video animation
+let accelerationAmount2 = 1;
+let scrollPosition2 = 0;
+let delay2 = 0;
+let sp = 0;
+
+sceneIntro2.on('update', (e) => {
+  if (e.scrollPos > 9000) {
+    sp = e.scrollPos % 9000;
+    scrollPosition2 = sp / 1000;
+    delay2 += (scrollPosition2 - delay2) * accelerationAmount2;
+    videoIntro2.currentTime = delay2;
+  }
+});
